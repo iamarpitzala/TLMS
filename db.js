c5b57b04589ec9e3c7885afc295f7f3b19b755ff
iamarpitzala <devarpit29@gmail.com>
2026-08-17 17:36:27 +0530

    clean up

--- db.js
+++ db.js
@@ -9,7 +9,6 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 15000,
